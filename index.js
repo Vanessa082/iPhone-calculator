79ee094021dd2feb7123811b5a0958db6b985b26
vanessa082 <wahvanessa22@gmail.com>
2023-12-13 21:21:05 +0100

    correcting linter

---
+++ index.js
@@ -0,0 +1,55 @@
+const display = document.getElementById("calc-display")
+const buttons = document.querySelectorAll("[data-number], [data-operation], [data-all-clear], [data-equals]")
+
+let firstOperand = ''
+let operator = ''
+let secondOperand = ''
+
+buttons.forEach(button => {
+  button.addEventListener("click", function () {
+    const value = this.innerText
+
+    if (value === "AC") {
+      display.value = ''
+      firstOperand = ''
+      operator = ''
+      secondOperand = ''
+    } else if (value === "+/-") {
+      display.value = (parseFloat(display.value) * -1).toString()
+    } else if (value === "%") {
+      display.value = (parseFloat(display.value) / 100).toString()
+    } else if (value === "=") {
+      if (firstOperand && operator && secondOperand) {
+        const result = calculate(parseFloat(firstOperand), operator, parseFloat(secondOperand))
+        display.value = result.toString()
+        firstOperand = result.toString()
+        operator = ''
+        secondOperand = ''
+      }
+    } else if (Number.isInteger(parseInt(value))) {
+      if (!operator) {
+        firstOperand += value
+      } else {
+        secondOperand += value
+      }
+      display.value = (operator ? secondOperand : firstOperand)
+    } else {
+      operator = value
+    }
+  });
+});
+
+function calculate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return parseFloat(num1 + num2)
+    case "-":
+      return parseFloat(num1 - num2)
+    case "×":
+      return parseFloat(num1 * num2)
+    case "÷":
+      return parseFloat(num1 / num2)
+    default:
+      return "Error"
+  }
+}
